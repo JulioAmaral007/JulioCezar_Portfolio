afdc088d7cd8d094ca52023be2fe43cf18541b31
Júlio Cézar <captaingamergroot@gmail.com>
2022-05-07 11:09:13 -0300

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".mobile").classList.toggle("show-menu")
+);
---
+++ README.md
@@ -0,0 +1,2 @@
+# JulioCezar_Portfolio
+ 
